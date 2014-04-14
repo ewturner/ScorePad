66893d62c95c1ec2ac2eea8471c92774a363fa5e
Eric Turner <eturner47@gmail.com>
2014-04-14 12:25:07 -0500

    Added fragment for new game screen

---
+++ app/src/main/java/edu/auburn/csse/comp3710/group14/NewGameFragment.java
@@ -0,0 +1,14 @@
+package edu.auburn.csse.comp3710.group14;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by eturner on 4/14/14.
+ */
+public class NewGameFragment extends Fragment {
+    public interface NewGameHost {
+        public void startGame();
+    }
+
+
+}

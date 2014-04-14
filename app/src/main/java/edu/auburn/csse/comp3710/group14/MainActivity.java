f26d139b6c824284335fd264ceb218248632c58e
Eric Turner <eturner47@gmail.com>
2014-04-14 10:38:06 -0500

    Initial commit

---
+++ app/src/main/java/edu/auburn/csse/comp3710/group14/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.auburn.csse.comp3710.group14;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+    }
+}
